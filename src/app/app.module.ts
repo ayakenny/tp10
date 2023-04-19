d8c720781dbb75d0e1fd797b27489a1190296109
AYAM Kenny <kenny.ayam@outlook.fr>
2023-04-19 22:31:42 +1100

    création du directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightDirectiveDirective } from './highlight-directive.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirectiveDirective
   ],
   imports: [
     BrowserModule,
